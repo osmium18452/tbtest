a3723d18c263de55906dd4c094929e2c4b94c9e6
17110 <1711042219@qq.com>
2023-05-10 17:50:44 +0800

    NO COMMENT

--- src/tb.jsx
+++ src/tb.jsx
@@ -65,207 +65,244 @@ export default () => {
     };
 
 
-    const reactorStatusHeader = [// sorter: (a, b) => a.value - b.value,
-        //     ...getColumnSearchProps('col1'),
+    const reactorStatusHeader = [
         {
-            title: '位号',
+            title: '位号 ',
             dataIndex: 'col1',
             key: 'col1',
             align: 'center',
-            width: 120, ...getColumnSearchProps('col1'),
+            width: 120,
+            fixed: 'left', ...getColumnSearchProps('col1'),
         },
         {
-            title: '裂解原料',
+            title: '预热器台数及型号',
             dataIndex: 'col2',
             key: 'col2',
             align: 'center',
-            width: 120, ...getColumnSearchProps('col2'),
+            width: 180, ...getColumnSearchProps('col2'),
         },
         {
-            title: '裂解炉目前情况（注1）', children: [
+            title: '加热介质情况', children: [
                 {
-                    title: '未改造',
-                    dataIndex: 'col3',
-                    key: 'col3',
-                    align: 'center',
-                    width: 90, ...getColumnSearchProps('col3'),
+                    title: '介质名称', children: [
+                        {
+                            title: 'LS ',
+                            dataIndex: 'col3',
+                            key: 'col3',
+                            align: 'center',
+                            width: 100, ...getColumnSearchProps('col3'),
+                        },
+                        {
+                            title: '凝液',
+                            dataIndex: 'col4',
+                            key: 'col4',
+                            align: 'center',
+                            width: 70, ...getColumnSearchProps('col4'),
+                        },
+                        {
+                            title: '急冷水',
+                            dataIndex: 'col5',
+                            key: 'col5',
+                            align: 'center',
+                            width: 80, ...getColumnSearchProps('col5'),
+                        },
+                        {
+                            title: '其它',
+                            dataIndex: 'col6',
+                            key: 'col6',
+                            align: 'center',
+                            width: 70, ...getColumnSearchProps('col6'),
+                        },
+                    ]
                 },
                 {
-                    title: '改造',
-                    dataIndex: 'col4',
-                    key: 'col4',
-                    align: 'center',
-                    width: 90, ...getColumnSearchProps('col4'),
+                    title: '入/出口温度', children: [
+                        {
+                            title: '℃',
+                            dataIndex: 'col7',
+                            key: 'col7',
+                            align: 'center',
+                            width: 120, ...getColumnSearchProps('col7'),
+                        },
+                    ]
+                },
+                {
+                    title: '流量', children: [
+                        {
+                            title: 't/h',
+                            dataIndex: 'col8',
+                            key: 'col8',
+                            align: 'center',
+                            width: 70, ...getColumnSearchProps('col8'),
+                        },
+                    ]
                 },
             ]
         },
         {
-            title: '投油量，kg/h',
-            dataIndex: 'col5',
-            key: 'col5',
-            align: 'center',
-            width: 150, ...getColumnSearchProps('col5'),
-        },
-        {
-            title: '稀释蒸汽，kg/h',
-            dataIndex: 'col6',
-            key: 'col6',
-            align: 'center',
-            width: 170, ...getColumnSearchProps('col6'),
-        },
-        {
-            title: '燃料量，kg/h ',
-            dataIndex: 'col7',
-            key: 'col7',
-            align: 'center',
-            width: 120, ...getColumnSearchProps('col7'),
-        },
-        {
-            title: 'BFW流量，kg/h',
-            dataIndex: 'col8',
-            key: 'col8',
-            align: 'center',
-            width: 150, ...getColumnSearchProps('col8'),
-        },
-        {
-            title: '进入裂解炉DS温度，℃', children: [
+            title: '空气预热后温度，℃', children: [
                 {
-                    title: '设计',
+                    title: '冬季',
                     dataIndex: 'col9',
                     key: 'col9',
                     align: 'center',
-                    width: 200, ...getColumnSearchProps('col9'),
+                    width: 70, ...getColumnSearchProps('col9'),
                 },
                 {
-                    title: '实际',
+                    title: '夏季',
                     dataIndex: 'col10',
                     key: 'col10',
                     align: 'center',
                     width: 70, ...getColumnSearchProps('col10'),
                 },
-            ]
-        },
-        {
-            title: 'DS过热后（如果有）温度，℃',
-            children: [
                 {
-                    title: '设计',
+                    title: '其他',
                     dataIndex: 'col11',
                     key: 'col11',
                     align: 'center',
                     width: 70, ...getColumnSearchProps('col11'),
                 },
+            ]
+        },
+        {
+            title: '投用后节约燃料', children: [
                 {
-                    title: '实际',
+                    title: 'kg/h',
                     dataIndex: 'col12',
                     key: 'col12',
                     align: 'center',
                     width: 70, ...getColumnSearchProps('col12'),
                 },
+                {
+                    title: '%',
+                    dataIndex: 'col13',
+                    key: 'col13',
+                    align: 'center',
+                    width: 70, ...getColumnSearchProps('col13'),
+                },
             ]
         },
         {
-            title: '蒸汽发生量，kg/h',
-            dataIndex: 'col13',
-            key: 'col13',
-            align: 'center',
-            width: 200, ...getColumnSearchProps('col13'),
-        },
-        {
-            title: 'SS 过热后温度,℃',
-            dataIndex: 'col14',
-            key: 'col14',
-            align: 'center',
-            width: 200, ...getColumnSearchProps('col14'),
-        },
-        {
-            title: '炉外壁平均温度，℃',
-            dataIndex: 'col15',
-            key: 'col15',
-            align: 'center',
-            width: 200, ...getColumnSearchProps('col15'),
-        },
-        {
-            title: '运转周期，天', children: [
+            title: '投用前/后排烟温度', children: [
                 {
-                    title: '设计',
+                    title: '℃',
+                    dataIndex: 'col14',
+                    key: 'col14',
+                    align: 'center',
+                    width: 180, ...getColumnSearchProps('col14'),
+                },
+            ]
+        },
+        {
+            title: '投用后燃烧器风门是否变化', children: [
+                {
+                    title: '是',
+                    dataIndex: 'col15',
+                    key: 'col15',
+                    align: 'center',
+                    width: 100, ...getColumnSearchProps('col15'),
+                },
+                {
+                    title: '否',
                     dataIndex: 'col16',
                     key: 'col16',
                     align: 'center',
-                    width: 170, ...getColumnSearchProps('col16'),
-                },
-                {
-                    title: '实际',
-                    dataIndex: 'col17',
-                    key: 'col17',
-                    align: 'center',
-                    width: 70, ...getColumnSearchProps('col17'),
+                    width: 100, ...getColumnSearchProps('col16'),
                 },
             ]
         },
         {
-            title: '运转周期控制因素', children: [
+            title: '投用时间',
+            dataIndex: 'col17',
+            key: 'col17',
+            align: 'center',
+            width: 100, ...getColumnSearchProps('col17'),
+        },
+        {
+            title: '进风口朝向', children: [
                 {
-                    title: '炉管壁温',
+                    title: '上',
                     dataIndex: 'col18',
                     key: 'col18',
                     align: 'center',
-                    width: 200, ...getColumnSearchProps('col18'),
+                    width: 70, ...getColumnSearchProps('col18'),
                 },
                 {
-                    title: 'TLE出口温度',
+                    title: '侧',
                     dataIndex: 'col19',
                     key: 'col19',
                     align: 'center',
-                    width: 150, ...getColumnSearchProps('col19'),
+                    width: 70, ...getColumnSearchProps('col19'),
                 },
                 {
-                    title: '文丘里压降',
+                    title: '下',
                     dataIndex: 'col20',
                     key: 'col20',
                     align: 'center',
-                    width: 150, ...getColumnSearchProps('col20'),
-                },
-                {
-                    title: '生产计划',
-                    dataIndex: 'col21',
-                    key: 'col21',
-                    align: 'center',
-                    width: 120, ...getColumnSearchProps('col21'),
+                    width: 70, ...getColumnSearchProps('col20'),
                 },
             ]
         },
         {
-            title: '是否预硫化', children: [
+            title: '是否设置过滤器', children: [
                 {
                     title: '是',
+                    dataIndex: 'col21',
+                    key: 'col21',
+                    align: 'center',
+                    width: 70, ...getColumnSearchProps('col21'),
+                },
+                {
+                    title: '否',
                     dataIndex: 'col22',
                     key: 'col22',
                     align: 'center',
                     width: 70, ...getColumnSearchProps('col22'),
                 },
+            ]
+        },
+        {
+            title: '吹扫频次，次/年',
+            dataIndex: 'col23',
+            key: 'col23',
+            align: 'center',
+            width: 150, ...getColumnSearchProps('col23'),
+        },
+        {
+            title: '投用前/后炉底负压值', children: [
                 {
-                    title: '否',
-                    dataIndex: 'col23',
-                    key: 'col23',
+                    title: 'mmH2O',
+                    dataIndex: 'col24',
+                    key: 'col24',
                     align: 'center',
-                    width: 70, ...getColumnSearchProps('col23'),
+                    width: 150, ...getColumnSearchProps('col24'),
                 },
             ]
         },
         {
-            title: '注硫剂类型',
-            dataIndex: 'col24',
-            key: 'col24',
-            align: 'center',
-            width: 150, ...getColumnSearchProps('col24'),
+            title: '发生泄漏情况', children: [
+                {
+                    title: '是',
+                    dataIndex: 'col25',
+                    key: 'col25',
+                    align: 'center',
+                    width: 70, ...getColumnSearchProps('col25'),
+                },
+                {
+                    title: '否',
+                    dataIndex: 'col26',
+                    key: 'col26',
+                    align: 'center',
+                    width: 70, ...getColumnSearchProps('col26'),
+                },
+            ]
         },
         {
-            title: '硫化物注入指标，PPM',
-            dataIndex: 'col25',
-            key: 'col25',
+            title: '存在问题及对策（如增加预热器后燃烧器如何调节）',
+            dataIndex: 'col27',
+            key: 'col27',
             align: 'center',
-            width: 240, ...getColumnSearchProps('col25'),
+            width: 240, ...getColumnSearchProps('col27'),
         }
     ];
 
@@ -282,7 +319,7 @@ export default () => {
 
     const host = "http://ys.nuclearsilo.me:8080/";
     // const host = '';
-    const url = host + '/infobase/tables/infotable7_1';
+    const url = host + '/infobase/tables/infotable10_1';
     useEffect(() => {
         async function fetchData() {
             const res = await fetch(url);
@@ -305,8 +342,9 @@ export default () => {
                 columns={tableHeader}
                 dataSource={reactorStatusTable}
                 size="small"
+                bordered
                 pagination={{size: 'small', position: ['bottomCenter'], pageSize: 20}}
-                scroll={{x: 1000}}
+                scroll={{x: 500}}
             />
             <div>
                 {comment}
