cdc2145095d16f491d90a5091b4662caa6558bb9
os <1711042219@qq.com>
2023-05-19 22:08:59 +0800

    no comment

--- src/tb.jsx
+++ src/tb.jsx
@@ -65,57 +65,64 @@ export default () => {
     };
 
 
-    const reactorStatusHeader = [
-        {title: '位号',dataIndex: 'col1',key: 'col1',align: 'center',width: 120,fixed:'left',...getColumnSearchProps('col1'),},
-        {title:'裂解气大阀型式',children:[
-        {title: '单闸板',dataIndex: 'col2',key: 'col2',align: 'center',width: 80,...getColumnSearchProps('col2'),},
-        {title: '双闸板',dataIndex: 'col3',key: 'col3',align: 'center',width: 80,...getColumnSearchProps('col3'),},
-        {title: '蝶阀',dataIndex: 'col4',key: 'col4',align: 'center',width: 70,...getColumnSearchProps('col4'),},
-        {title: '楔形阀',dataIndex: 'col5',key: 'col5',align: 'center',width: 80,...getColumnSearchProps('col5'),},
-        {title: '其它',dataIndex: 'col6',key: 'col6',align: 'center',width: 70,...getColumnSearchProps('col6'),},
+    const reactorStatusHeader =[
+        {title: '位号',dataIndex: 'col1',key: 'col1',align: 'center',fixed:'left',width: 120,...getColumnSearchProps('col1'),},
+        {title:'减温器',align:'center',children:[
+        {title:'减温器本体及安装形式',align:'center',children:[
+                {title:'类型',align:'center',children:[
+                        {title: '一体',dataIndex: 'col2',key: 'col2',align: 'center',width: 70,...getColumnSearchProps('col2'),},
+                        {title: '分体',dataIndex: 'col3',key: 'col3',align: 'center',width: 70,...getColumnSearchProps('col3'),},
+                    ]},
+                {title:'插入式',align:'center',children:[
+                        {title: '喷嘴',dataIndex: 'col4',key: 'col4',align: 'center',width: 70,...getColumnSearchProps('col4'),},
+                    ]},
+                {title:'在线式',align:'center',children:[
+                        {title: '可变面积',dataIndex: 'col5',key: 'col5',align: 'center',width: 100,...getColumnSearchProps('col5'),},
+                        {title: '文丘里',dataIndex: 'col6',key: 'col6',align: 'center',width: 80,...getColumnSearchProps('col6'),},
+                        {title: '其他',dataIndex: 'col7',key: 'col7',align: 'center',width: 70,...getColumnSearchProps('col7'),},
+                        {title: '安装方式',dataIndex: 'col8',key: 'col8',align: 'center',width: 100,...getColumnSearchProps('col8'),},
+                    ]},
+                {title:'减温器本体',align:'center',children:[
+                        {title: '材质',dataIndex: 'col9',key: 'col9',align: 'center',width: 70,...getColumnSearchProps('col9'),},
+                        {title: '型号',dataIndex: 'col10',key: 'col10',align: 'center',width: 100,...getColumnSearchProps('col10'),},
+                        {title: '生产厂家',dataIndex: 'col11',key: 'col11',align: 'center',width: 300,...getColumnSearchProps('col11'),},
+                    ]},
+                {title:'水阀',align:'center',children:[
+                        {title: '型号',dataIndex: 'col12',key: 'col12',align: 'center',width: 70,...getColumnSearchProps('col12'),},
+                        {title: '生产厂家',dataIndex: 'col13',key: 'col13',align: 'center',width: 100,...getColumnSearchProps('col13'),},
+                    ]},
+                {title: '投用年月',dataIndex: 'col14',key: 'col14',align: 'center',width: 100,...getColumnSearchProps('col14'),},
             ]},
-        {title:'清焦阀型式',children:[
-        {title: '单闸板',dataIndex: 'col7',key: 'col7',align: 'center',width: 80,...getColumnSearchProps('col8'),},
-        {title: '双闸板',dataIndex: 'col8',key: 'col8',align: 'center',width: 80,...getColumnSearchProps('col9'),},
-        {title: '蝶阀',dataIndex: 'col9',key: 'col9',align: 'center',width: 70,...getColumnSearchProps('col10'),},
-        {title: '楔形阀',dataIndex: 'col10',key: 'col10',align: 'center',width: 80,...getColumnSearchProps('col11'),},
-        {title: '其它',dataIndex: 'col11',key: 'col11',align: 'center',width: 70,...getColumnSearchProps('col12'),},
+        {title: '存在问题及对策',dataIndex: 'col15',key: 'col15',align: 'center',width: 180,...getColumnSearchProps('col15'),},
             ]},
-        {title: '生产厂家',dataIndex: 'col12',key: 'col12',align: 'center',width: 100,...getColumnSearchProps('col13'),},
-        {title:'裂解气大阀配制',children:[
-        {title: '单阀',dataIndex: 'col13',key: 'col13',align: 'center',width: 70,...getColumnSearchProps('col14'),},
-        {title: '双阀',dataIndex: 'col14',key: 'col14',align: 'center',width: 70,...getColumnSearchProps('col15'),},
+        {title:'液体原料料流量仪表',align:'center',children:[
+                {title:'',align:'center',children:[
+                        {title:'高温（是否带旁路）',align:'center',children:[
+        {title: '是',dataIndex: 'col16',key: 'col16',align: 'center',width: 90,...getColumnSearchProps('col16'),},
+        {title: '否',dataIndex: 'col17',key: 'col17',align: 'center',width: 90,...getColumnSearchProps('col17'),},
+                            ]},
+                        {title:'普通（是否带旁路）',align:'center',children:[
+        {title: '是',dataIndex: 'col18',key: 'col18',align: 'center',width: 90,...getColumnSearchProps('col18'),},
+        {title: '否',dataIndex: 'col19',key: 'col19',align: 'center',width: 90,...getColumnSearchProps('col19'),},
+                            ]},
+        {title: '型号',dataIndex: 'col20',key: 'col20',align: 'center',width: 150,...getColumnSearchProps('col20'),},
+        {title: '生产厂家',dataIndex: 'col21',key: 'col21',align: 'center',width: 150,...getColumnSearchProps('col21'),},
+        {title: '存在问题及建议',dataIndex: 'col22',key: 'col22',align: 'center',width: 180,...getColumnSearchProps('col22'),},
+                    ]},
+                {title:'',align:'center',children:[
+                        {title:'高温（是否带旁路）',align:'center',children:[
+        {title: '是',dataIndex: 'col23',key: 'col23',align: 'center',width: 90,...getColumnSearchProps('col23'),},
+        {title: '否',dataIndex: 'col24',key: 'col24',align: 'center',width: 90,...getColumnSearchProps('col24'),},
+                            ]},
+                        {title:'普通（是否带旁路）',align:'center',children:[
+        {title: '是',dataIndex: 'col25',key: 'col25',align: 'center',width: 90,...getColumnSearchProps('col25'),},
+        {title: '否',dataIndex: 'col26',key: 'col26',align: 'center',width: 90,...getColumnSearchProps('col26'),},
+                            ]},
+        {title: '型号',dataIndex: 'col27',key: 'col27',align: 'center',width: 70,...getColumnSearchProps('col27'),},
+        {title: '生产厂家',dataIndex: 'col28',key: 'col28',align: 'center',width: 100,...getColumnSearchProps('col28'),},
+        {title: '存在问题及对策',dataIndex: 'col29',key: 'col29',align: 'center',width: 180,...getColumnSearchProps('col29'),},
+                    ]},
             ]},
-        {title:'清焦阀配制',children:[
-        {title: '单阀',dataIndex: 'col15',key: 'col15',align: 'center',width: 70,...getColumnSearchProps('col16'),},
-        {title: '双阀',dataIndex: 'col16',key: 'col16',align: 'center',width: 70,...getColumnSearchProps('col17'),},
-            ]},
-        {title:'防焦蒸汽种类及温度(℃)/压力(Mpa(G))',children:[
-        {title: 'LS(温度/压力)',dataIndex: 'col17',key: 'col17',align: 'center',width: 150,...getColumnSearchProps('col18'),},
-        {title: 'DS(温度/压力)',dataIndex: 'col18',key: 'col18',align: 'center',width: 150,...getColumnSearchProps('col19'),},
-        {title: 'MS(温度/压力)',dataIndex: 'col19',key: 'col19',align: 'center',width: 150,...getColumnSearchProps('col20'),},
-            ]},
-        {title:'开/关一次用时',children:[
-        {title: 'min',dataIndex: 'col20',key: 'col20',align: 'center',width: 120,...getColumnSearchProps('col21'),},
-            ]},
-        {title:'与清焦阀联动方式',children:[
-        {title: '机械连接',dataIndex: 'col21',key: 'col21',align: 'center',width: 100,...getColumnSearchProps('col22'),},
-        {title: '电子联锁',dataIndex: 'col22',key: 'col22',align: 'center',width: 100,...getColumnSearchProps('col23'),},
-        {title: '手动',dataIndex: 'col23',key: 'col23',align: 'center',width: 70,...getColumnSearchProps('col24'),},
-            ]},
-        {title:'电子联锁压力联锁条件',children:[
-        {title: '3取2',dataIndex: 'col24',key: 'col24',align: 'center',width: 70,...getColumnSearchProps('col25'),},
-        {title: '2取2',dataIndex: 'col25',key: 'col25',align: 'center',width: 70,...getColumnSearchProps('col26'),},
-        {title: '2取1',dataIndex: 'col26',key: 'col26',align: 'center',width: 70,...getColumnSearchProps('col27'),},
-            ]},
-        {title:'裂解气大阀是否全开才注入急冷油',children:[
-        {title: '是',dataIndex: 'col27',key: 'col27',align: 'center',width: 70,...getColumnSearchProps('col28'),},
-        {title: '否',dataIndex: 'col28',key: 'col28',align: 'center',width: 70,...getColumnSearchProps('col29'),},
-            ]},
-        {title: '大阀底部排液频率',dataIndex: 'col29',key: 'col29',align: 'center',width: 180,...getColumnSearchProps('col30'),},
-        {title: '防焦蒸汽检查频率/巡查频率',dataIndex: 'col30',key: 'col30',align: 'center',width: 220,...getColumnSearchProps('col31'),},
-        {title: '清焦时大阀前后控制压差，Mpa',dataIndex: 'col31',key: 'col31',align: 'center',width: 220,...getColumnSearchProps('col32'),},
-        {title: '存在的问题及对策',dataIndex: 'col32',key: 'col32',align: 'center',width: 150,...getColumnSearchProps('col32'),},
     ]
 
 
@@ -129,9 +136,9 @@ export default () => {
     const [searchText, setSearchText] = useState('');
     const [searchedColumn, setSearchedColumn] = useState(reactorComment);
 
-    const host = "http://ys.nuclearsilo.me:8080/";
+    const host = "http://10.176.26.86:20127";
     // const host = '';
-    const url = host + '/infobase/tables/infotable13_1';
+    const url = host + '/infobase/tables/infotable19_1';
     useEffect(() => {
         async function fetchData() {
             const res = await fetch(url);
@@ -155,7 +162,7 @@ export default () => {
                 dataSource={reactorStatusTable}
                 size="small"
                 bordered
-                pagination={{size: 'small', position: ['bottomCenter'], pageSize: 20}}
+                pagination={{size: 'small', position: ['bottomCenter'], pageSize: 80}}
                 scroll={{x: 500}}
             />
             <div>
